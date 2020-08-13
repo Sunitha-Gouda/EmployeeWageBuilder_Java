dbaa6c04b5814a1cd841dbdd7d8fc0f1637e8687
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-13 13:43:51 +0530

    Part time Employee Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,20 +6,33 @@ public class EmployeeWageBuilder
 	//Display welcome message
 		System.out.println("Welcome To Employee Wage Computation Problem");
 	//Constants
-		int IS_PRESENT = 1;
+		int IS_FULL_TIME = 1, IS_PART_TIME = 2;
 		int EMP_WAGE_PER_HR=20;
-		int FULL_DAY_HR=8;
 	//Computation
-		double empCheck = Math.floor(Math.random() * 10 ) % 2;
-		if (empCheck== IS_PRESENT)
+		double empCheck = Math.floor(Math.random() * 10 ) % 3;
+		if (empCheck == IS_FULL_TIME)
 		{
-				System.out.println("Employee Is Present");
-			int Salary = EMP_WAGE_PER_HR * FULL_DAY_HR;
-				System.out.println("Salary="+Salary);
+				System.out.println("Employee is  Full time Employee");
+					int empHrs=8;
+					int Salary = EMP_WAGE_PER_HR * empHrs;
+				System.out.println("Salary = " +Salary);
+		}
+		else if (empCheck == IS_PART_TIME)
+		{
+				System.out.println("Employee is Part time Employee");
+					int empHrs=4;
+					int Salary = EMP_WAGE_PER_HR * empHrs;
+            System.out.println("Salary = " +Salary);
 		}
      	else
 		{
 			System.out.println("Employee Is Absent");
+					int empHrs=0;
+			   	int Salary = EMP_WAGE_PER_HR * empHrs;
+			System.out.println("Salary = " +Salary);
+
 		}
+
 	}
 }
+
