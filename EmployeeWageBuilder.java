71b320bd04601ff95316369c3c5789c246aeb50e
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-13 11:46:33 +0530

    Check Employee is Present or Absent

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,5 +6,14 @@ public class EmployeeWageBuilder
 	{
 		//Display welcome message
 		System.out.println("Welcome To Employee Wage Computation Problem");
+		 	//Constants
+      	int IS_PRESENT = 1;
+         //Computation
+      	double empCheck = Math.floor(Math.random() * 10 ) % 2;
+			if (empCheck== IS_PRESENT)
+         	System.out.println("Employee Is Present");
+      	else
+         	System.out.println("Employee Is Absent");
+
 	}
 }
