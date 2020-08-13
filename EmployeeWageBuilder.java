d1abeaed2ad55ae6b0491e6339e83dd66b44f3f0
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-13 16:04:23 +0530

    Montly wages of Employee

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,36 +3,43 @@ public class EmployeeWageBuilder
 {
 	public static void main(String[] args)
 	{
-	//Display welcome message
-		System.out.println("Welcome To Employee Wage Computation Problem");
-	//Constants
-		int IS_FULL_TIME = 1, IS_PART_TIME = 2;
-		int EMP_WAGE_PER_HR=20;
-	//Computation
-		double empCheck = Math.floor(Math.random() * 10 ) % 3;
-		if (empCheck == IS_FULL_TIME)
-		{
+		//Display welcome message
+			System.out.println("Welcome To Employee Wage Computation Problem");
+		//Constants
+			int IS_FULL_TIME = 1, IS_PART_TIME = 2;
+			int EMP_WAGE_PER_HR=20;
+			int NUM_OF_WORKING_DAYS=20;
+			int empHrs;
+			int Salary,TotalSalary=0 ;
+		//Computation
+			for ( int days=0; days<NUM_OF_WORKING_DAYS; days++)
+			{
+
+				double empCheck = Math.floor(Math.random() * 10 ) % 3;
+
+				if (empCheck == IS_FULL_TIME)
+				{
+					empHrs=8;
 				System.out.println("Employee is  Full time Employee");
-					int empHrs=8;
-					int Salary = EMP_WAGE_PER_HR * empHrs;
-				System.out.println("Salary = " +Salary);
-		}
-		else if (empCheck == IS_PART_TIME)
-		{
+
+				}
+				else if (empCheck == IS_PART_TIME)
+				{
+					empHrs=4;
 				System.out.println("Employee is Part time Employee");
-					int empHrs=4;
-					int Salary = EMP_WAGE_PER_HR * empHrs;
-            System.out.println("Salary = " +Salary);
-		}
-     	else
-		{
-			System.out.println("Employee Is Absent");
-					int empHrs=0;
-			   	int Salary = EMP_WAGE_PER_HR * empHrs;
-			System.out.println("Salary = " +Salary);
+				}
+     			else
+				{
+					empHrs=0;
+				System.out.println("Employee Is Absent");
+				}
 
-		}
-
+				Salary=(NUM_OF_WORKING_DAYS * empHrs);
+				System.out.println("Salary=" +Salary);
+				TotalSalary=( TotalSalary+Salary);
+				System.out.println("TotalSalary = " +TotalSalary );
+			}
 	}
+
 }
 
