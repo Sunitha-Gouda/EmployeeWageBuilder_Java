26223958a6ed9ee305965b2a8567ad6e47ae0930
Sunitha-Gouda <sunitha.pink97@gmail.com>
2020-08-13 11:38:43 +0530

    Employee Wage Computattion Problem

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,10 @@
+
+public class EmployeeWageBuilder
+{
+
+	public static void main(String[] args)
+	{
+		//Display welcome message
+		System.out.println("Welcome To Employee Wage Computation Problem");
+	}
+}
